f9bf21df0e6c5d7ff34b8696f7174ef3cbf4d030
Jason Edstrom <JasonE@magenic.com>
2024-02-27 19:29:44 -0600

    feat(import): initial files for endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
+import { ImportModule } from './import/import.module';
 import { SharedModule } from './shared/shared.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [SharedModule, UserModule, AuthModule, ArticleModule],
+  imports: [SharedModule, UserModule, AuthModule, ArticleModule, ImportModule],
   controllers: [AppController],
   providers: [AppService],
 })
